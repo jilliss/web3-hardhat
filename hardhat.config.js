a1436aba75139c0eedc8c75f4d25a365227458fa
jilliss <zared9802@gmail.com>
2025-05-23 20:59:22 +0800

    init

---
+++ hardhat.config.js
@@ -0,0 +1,47 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+
+const { vars } = require("hardhat/config");
+// Go to https://alchemy.com, sign up, create a new App in
+// its dashboard, and store it as the "ALCHEMY_API_KEY"
+// configuration variable
+const ALCHEMY_API_KEY = vars.get("ALCHEMY_API_KEY");
+
+// Replace this private key with your Sepolia account private key
+// To export your private key from Coinbase Wallet, go to
+// Settings > Developer Settings > Show private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Store the private key as the "SEPOLIA_PRIVATE_KEY" configuration
+// variable.
+// Beware: NEVER put real Ether into testing accounts
+const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
+
+const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.28",
+
+  // ...rest of your config...
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY,
+    },
+  },
+};
+
+
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
